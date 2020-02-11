568a8c405b64a3a40000ece1fed81cf1b9bca483
Michael Sestito <mvsestito@gmail.com>
2020-02-10 18:22:45 -0800

    use /Users/michael_sestito instead of tilda and add auto ctags file linking for all files in ctags dir

--- setup.sh
+++ setup.sh
@@ -37,67 +37,74 @@ echo "Brew packages successfully installed"
 ## Setup symlinks to dotfiles
 #### bash config
 echo "Configuring bash dotfiles..."
-ln -sf ~/bin/bash/bashrc ~/.bashrc
-ln -sf ~/bin/bash/bash_profile ~/.bash_profile
+ln -sf $HOME/bin/bash/bashrc $HOME/.bashrc
+ln -sf $HOME/bin/bash/bash_profile $HOME/.bash_profile
 
 ## create ctags file
 # run "ctags -R ." on project directory root to build tags file
-echo "Linking ctags file..."
-ln -sf ~/bin/ctags/dotctags ~/.ctags
+echo "Linking ctags files..."
+# create directory if not exists
+mkdir -p $HOME/.ctags.d
+for f in $HOME/bin/ctags/* do
+	OUT_PATH="$HOME/.ctags.d/$(echo $f | cut -d'/' -f6)"
+	echo "linking $f to $OUT_PATH"
+
+	ln -sf $f $OUT_PATH
+done
 
 ### ssh config
 echo "Configuring ssh dotfiles..."
 # [ operator is a conditional test. shorthand for "test" keyword cmd
 # check for existing sym link and rm it if exist
-[ -L ~/.ssh/config ] && rm ~/.ssh/config
-[ -f ~/.ssh/config ] && echo "~/.ssh/config exists. Delete and re-run setup." && exit
-mkdir -p ~/.ssh
-ln ~/bin/ssh/config ~/.ssh/config
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/config
+[ -L $HOME/.ssh/config ] && rm $HOME/.ssh/config
+[ -f $HOME/.ssh/config ] && echo "$HOME/.ssh/config exists. Delete and re-run setup." && exit
+mkdir -p $HOME/.ssh
+ln $HOME/bin/ssh/config $HOME/.ssh/config
+chmod 700 $HOME/.ssh
+chmod 600 $HOME/.ssh/config
 
 #### git config
 echo "Configuring git dotfiles..."
-ln -sf ~/bin/git/gitconfig ~/.gitconfig
+ln -sf $HOME/bin/git/gitconfig $HOME/.gitconfig
 
 #### tmux config
 #echo "Configuring tmux dotfiles..."
 # check for existing sym link and rm it if exist
-#[ -L ~/.tmux ] && rm ~/.tmux
-#[ -d ~/.tmux ] && echo "~/.tmux exists. Delete and re-run setup." && exit
-#ln -sf ~/bin/tmux ~/.tmux
-#ln -sf ~/bin/tmux/tmux.conf ~/.tmux.conf
-#mkdir -p ~/.tmux/plugins
-#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-#~/.tmux/plugins/tpm/bin/install_plugins
+#[ -L $HOME/.tmux ] && rm $HOME/.tmux
+#[ -d $HOME/.tmux ] && echo "$HOME/.tmux exists. Delete and re-run setup." && exit
+#ln -sf $HOME/bin/tmux $HOME/.tmux
+#ln -sf $HOME/bin/tmux/tmux.conf $HOME/.tmux.conf
+#mkdir -p $HOME/.tmux/plugins
+#git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+#$HOME/.tmux/plugins/tpm/bin/install_plugins
 
 #### vim config
 echo "Configuring vim dotfiles..."
 # check for existing sym link and rm it if exist
-[ -L ~/.vim ] && rm ~/.vim
+[ -L $HOME/.vim ] && rm $HOME/.vim
 # check if real dir
-[ -d ~/.vim ] && echo "~/.vim exists. Delete and re-run setup." && exit
-[ -L ~/.local/share/nvim/site/autoload ] && rm ~/.local/share/nvim/site/autoload
-[ -d ~/.local/share/nvim/site/autoload ] && echo "~/.local/share/nvim/site/autoload exists. Delete and re-run setup." && exit
-[ -L ~/.config/nvim/init.vim ] && rm ~/.config/nvim/init.vim
+[ -d $HOME/.vim ] && echo "$HOME/.vim exists. Delete and re-run setup." && exit
+[ -L $HOME/.local/share/nvim/site/autoload ] && rm $HOME/.local/share/nvim/site/autoload
+[ -d $HOME/.local/share/nvim/site/autoload ] && echo "$HOME/.local/share/nvim/site/autoload exists. Delete and re-run setup." && exit
+[ -L $HOME/.config/nvim/init.vim ] && rm $HOME/.config/nvim/init.vim
 # check if real file
-[ -f ~/.config/nvim/init.vim ] && echo "~/.config/nvim/init.vim exists. Delete and re-run setup." && exit
+[ -f $HOME/.config/nvim/init.vim ] && echo "$HOME/.config/nvim/init.vim exists. Delete and re-run setup." && exit
 # check for plugged dir
-[ -d ~/.config/nvim/plugged ] && echo "~/.config/nvim/plugged exists. Delete and re-run setup." && exit
+[ -d $HOME/.config/nvim/plugged ] && echo "$HOME/.config/nvim/plugged exists. Delete and re-run setup." && exit
 
-mkdir -p ~/.vim/autoload
-mkdir -p ~/.vim/colors
-mkdir -p ~/.local/share/nvim/site/autoload
+mkdir -p $HOME/.vim/autoload
+mkdir -p $HOME/.vim/colors
+mkdir -p $HOME/.local/share/nvim/site/autoload
 
-ln -sf ~/bin/nvim ~/.vim
-ln -sf ~/bin/nvim/init.vim ~/.vim/.vimrc
-ln -sf ~/bin/nvim/colors/molokai.vim ~/.vim/colors/molokai.vim
-ln -sf ~/bin/nvim/autoload/plug.vim ~/.vim/autoload/plug.vim
+ln -sf $HOME/bin/nvim $HOME/.vim
+ln -sf $HOME/bin/nvim/init.vim $HOME/.vim/.vimrc
+ln -sf $HOME/bin/nvim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
+ln -sf $HOME/bin/nvim/autoload/plug.vim $HOME/.vim/autoload/plug.vim
 
 # symlinks for local neovim
-ln -sf ~/.vim/autoload/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
-mkdir -p ~/.config/nvim
-ln -sf ~/.vim/.vimrc ~/.config/nvim/init.vim
+ln -sf $HOME/.vim/autoload/plug.vim $HOME/.local/share/nvim/site/autoload/plug.vim
+mkdir -p $HOME/.config/nvim
+ln -sf $HOME/.vim/.vimrc $HOME/.config/nvim/init.vim
 
 #### install pip packages
 echo "Installing pip packages..."
@@ -111,7 +118,7 @@ echo "Installing vim plugins..."
 nvim -c "PlugInstall | PlugClean | qall"
 
 #### source colorscheme
-nvim -c "source ~/.vim/colors/molokai.vim | qall"
+nvim -c "source $HOME/.vim/colors/molokai.vim | qall"
 
 echo "dotfiles setup complete!"
 
